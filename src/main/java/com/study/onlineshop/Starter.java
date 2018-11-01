702b64e7d48ae61664dbbe46f4a51d7b90a3acf6
Tolik Trubin <trubintolik@gmail.com>
2018-11-01 11:26:27 +0200

    add login page

--- src/main/java/com/study/onlineshop/Starter.java
+++ src/main/java/com/study/onlineshop/Starter.java
@@ -2,11 +2,15 @@ package com.study.onlineshop;
 
 import com.study.onlineshop.dao.jdbc.JdbcProductDao;
 import com.study.onlineshop.service.impl.DefaultProductService;
+import com.study.onlineshop.web.servlet.LoginServlet;
 import com.study.onlineshop.web.servlet.ProductsApiServlet;
 import com.study.onlineshop.web.servlet.ProductsServlet;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Starter {
     public static void main(String[] args) throws Exception {
@@ -16,10 +20,15 @@ public class Starter {
         // configure services
         DefaultProductService defaultProductService = new DefaultProductService(jdbcProductDao);
 
+        // store
+        List<String> activeTokens = new ArrayList<>();
+
         // servlets
         ProductsServlet productsServlet = new ProductsServlet();
         productsServlet.setProductService(defaultProductService);
+        productsServlet.setActiveTokens(activeTokens);
         ProductsApiServlet productsApiServlet = new ProductsApiServlet(defaultProductService);
+
 
         // config web server
         ServletContextHandler servletContextHandler = new ServletContextHandler();
@@ -27,6 +36,7 @@ public class Starter {
         servletContextHandler.addServlet(new ServletHolder(productsServlet), "/");
 
         servletContextHandler.addServlet(new ServletHolder(productsApiServlet), "/api/v1/products");
+        servletContextHandler.addServlet(new ServletHolder(new LoginServlet(activeTokens)), "/login");
 
         Server server = new Server(8080);
         server.setHandler(servletContextHandler);
--- src/main/java/com/study/onlineshop/web/servlet/ProductsServlet.java
+++ src/main/java/com/study/onlineshop/web/servlet/ProductsServlet.java
@@ -5,6 +5,7 @@ import com.study.onlineshop.service.ProductService;
 import com.study.onlineshop.web.templater.PageGenerator;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,20 +15,43 @@ import java.util.List;
 
 public class ProductsServlet extends HttpServlet {
     private ProductService productService;
+    private List<String> activeTokens;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PageGenerator pageGenerator = PageGenerator.instance();
-        List<Product> products = productService.getAll();
+        Cookie[] cookies = req.getCookies();
+        boolean isAuth = false;
 
-        HashMap<String, Object> parameters = new HashMap<>();
-        parameters.put("products", products);
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("user-token")) {
+                    if (activeTokens.contains(cookie.getValue())) {
+                        isAuth = true;
+                    }
+                    break;
+                }
+            }
+        }
 
-        String page = pageGenerator.getPage("products", parameters);
-        resp.getWriter().write(page);
+        if (isAuth) {
+            PageGenerator pageGenerator = PageGenerator.instance();
+            List<Product> products = productService.getAll();
+
+            HashMap<String, Object> parameters = new HashMap<>();
+            parameters.put("products", products);
+
+            String page = pageGenerator.getPage("products", parameters);
+            resp.getWriter().write(page);
+        } else {
+            resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
     }
 
     public void setProductService(ProductService productService) {
         this.productService = productService;
+    }
+
+    public void setActiveTokens(List<String> activeTokens) {
+        this.activeTokens = activeTokens;
     }
 }
